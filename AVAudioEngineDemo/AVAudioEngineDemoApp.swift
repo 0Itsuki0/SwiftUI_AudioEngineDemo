3d61e0a719de669769f7d7a28f0d1529f1c6200a
itsuki <itsuki.enjyoy@gmail.com>
2025-08-29 09:49:07 +0900

    Initial Commit

---
+++ AVAudioEngineDemo/AVAudioEngineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVAudioEngineDemoApp.swift
+//  AVAudioEngineDemo
+//
+//  Created by Itsuki on 2025/08/29.
+//
+
+import SwiftUI
+
+@main
+struct AVAudioEngineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
